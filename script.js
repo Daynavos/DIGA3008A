78ed0184f80a877063c2e526fb4f41e7f9d2df68
Daynavos <dayna22vos@gmail.com>
2025-05-20 12:57:45 +0200

    jumpt to top

--- script.js
+++ script.js
@@ -23,4 +23,15 @@ document.addEventListener("DOMContentLoaded", () => {
       lastScrollY = window.scrollY;
     });
   }
+
+  const btn = document.getElementById("top");
+
+  window.addEventListener("scroll", () => {
+    btn.classList.toggle("show", window.scrollY > 300);
+  });
+
+  btn.addEventListener("click", (e) => {
+    e.preventDefault();
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  });
 });
